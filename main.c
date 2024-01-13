a0db6a04470c1cb8a46e5677a011a83853b087c5
Drishti <drishti.gup@gmail.com>
2024-01-13 16:34:19 -0600

    part 2 complete for intToInt hashmap

--- main.c
+++ main.c
@@ -1,8 +1,59 @@
 #include "hashmap.h"
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* tokens [3]; 
+void splitStringBySpace(char* input) {
+    char* token = strtok(input, " ");
+	int i = 0;
+	//char* tokens [3];  
+    while (token != NULL) {
+		tokens[i] = token;
+		i++;
+		token = strtok(NULL, " ");
+		
+    }
+}
 
 int main(int argc, char** argv){
+	struct IntToIntHashMap* h1 = createIntToIntHashMap(10);
+	while (true){
+
+		char command[20];
+		int number1;
+		int number2;
+
+		printf("Get <Key> or Put <Key> <Value>. Examples include \"Get 9\" or \"Put 8 10\". If you want to exit, enter EXIT: \n");
+		scanf("%s", command);
+		if (strcmp(command, "Get") == 0){
+			scanf("%d", &number1); 
+			int value = intToIntHashMapGet(h1, number1);
+			if (value != 0){
+				printf("Found here is the key value pair!: {%d, %d}\n", number1, value);
+			}
+			else {
+				fprintf(stderr, "The following key is not found in the hashmap!: %d\n", number1);
+			}
+
+		}
+		else if (strcmp(command, "Put") == 0){
+			scanf("%d%d", &number1, &number2); 
+			intToIntHashMapInsert(h1, number1, number2);
+			
+		}
+		else {
+			return 0;
+		}
+	}
+
+
+
+
+
+	}
+	/*
 	//while(true){
 		struct IntToIntHashMap* h1 = createIntToIntHashMap(10);
 		printf("initial hashtable\n\n");
@@ -34,4 +85,5 @@ int main(int argc, char** argv){
 		
 		//TODO: fill this in
 	//}
-}
+	*/
+
